bcb8c5818a9a17cb16a66b76cbf5f1e05c785911
simontijanic <simontijanicerkul@gmail.com>
2025-01-31 12:48:33 +0100

    admin seeding for ENV

--- controllers/dbController.js
+++ controllers/dbController.js
@@ -4,6 +4,15 @@ const connectToDb = async () => {
     try {
         await mongoose.connect(process.env.MONGODBURL)
         console.log("Connected to database")
+
+        if (process.env.seedAdmin == "true"){
+            const Admin = require("../models/adminModel")
+            const admin = new Admin({
+                username: "admin",
+                password: "admin"
+            })
+            await admin.save()
+        }
     } catch(err) {
         console.log(err)
     }
